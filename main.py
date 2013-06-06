781507551d3b42cc53170c1f93fbac1ef3964b62
Emmanuel K <eklu@hawk.iit.edu>
2013-06-05 22:24:19 -0500

    appengine updates

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import os
-from google.appengine.ext import webapp
-from google.appengine.ext.webapp import util
+import webapp2
 from google.appengine.ext.webapp import template
 
-class MainHandler(webapp.RequestHandler):
+class MainPage(webapp2.RequestHandler):
   def get (self, q):
     if q is None:
       q = 'index.html'
@@ -12,9 +11,4 @@ class MainHandler(webapp.RequestHandler):
     self.response.headers ['Content-Type'] = 'text/html'
     self.response.out.write (template.render (path, {}))
 
-def main ():
-  application = webapp.WSGIApplication ([('/(.*html)?', MainHandler)], debug=True)
-  util.run_wsgi_app (application)
-
-if __name__ == '__main__':
-  main ()
+app = webapp2.WSGIApplication ([('/(.*html)?', MainPage)])
